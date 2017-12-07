e9c593073550361713d14b19fcce950a0a5d1e51
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-12-06 21:13:16 -0500

    first push to database

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,22 @@
 CREATE DATABASE Doodle_db;
 USE Doodle_db;
 
+## Table to hold answers to be used as prompts for drawing
+
 CREATE TABLE answers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(15) NOT NULL,
-	description varchar(50) NOT NULL,
+	description varchar(50) NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE cats
+## Table to hold all user information, auto-generated user information as well as customized with name and password
+
+CREATE TABLE users
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	name varchar(15) NULL,
+	password varchar(15) NULL,
 	PRIMARY KEY (id)
 );
