083e7ceaaea50ab2598ae5161a1d095d3f9a15b0
Ovieh <ovieh.mosley@gmail.com>
2017-12-09 01:58:48 -0500

    working on chat

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
 app.use(express.static("./public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,3 +23,10 @@ app.listen(PORT, () => {
 });
 
 app.use("/", routes);
+
+io.on("connection", socket => {
+    socket.emit("news", {hello: "world"});
+    socket.on("my other event", data => {
+        console.log(data);
+    });
+});
