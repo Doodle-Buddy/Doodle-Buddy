d46b740012c21a46ce8316088536f5a31e2c2a26
perez6736 <perez6736@gmail.com>
2017-12-16 01:15:27 -0500

    new logic to starting game... delete users that disconnect

--- server.js
+++ server.js
@@ -72,10 +72,12 @@ var drawer;
 var drawerID;
 var round = 0;
 var userGuess;
+var isGameReady = true;
 
 // this is a new connection trigger for the socket
 io.sockets.on("connection", function(socket){
     console.log("connection made!")
+
     // putting game function here. cause it needs the socket param. 
     function aRound(){
         console.log("the round method is called. ");
@@ -115,6 +117,7 @@ io.sockets.on("connection", function(socket){
             }
             else{
                 io.emit("chat message", "Game is over.");
+                isGameStarted = false;
             }
         });
     }
@@ -128,6 +131,37 @@ io.sockets.on("connection", function(socket){
     socket.on("chat message", msg => {
         io.emit("chat message", msg);
         console.log(`message: ${msg}`);
+
+        if(isGameReady){
+            for(i=0; i<msg.length; i++){
+                if(msg.charAt(i) === ":"){
+                    var startGameMsg = msg.substring(i+1, msg.length).trim().toLowerCase();
+        
+                    // if they said
+                    if(startGameMsg === "start game"){
+                        io.emit("chat message", "Game on!");
+                        aRound();
+                        isGameReady = false;
+                        
+                    }
+                }
+            }
+        }
+
+        if(!isGameReady){
+            for(i=0; i<msg.length; i++){
+                if(msg.charAt(i) === ":"){
+                    var startGameMsg = msg.substring(i+1, msg.length).trim().toLowerCase();
+        
+                    // if they said
+                    if(startGameMsg === "end game"){
+                        io.emit("chat message", "Game ended!");
+                        isGameReady = true;
+                    }
+                }
+            }
+        }
+
     });
 
     socket.on("clear", function (data) {
@@ -155,16 +189,16 @@ io.sockets.on("connection", function(socket){
         }
         console.log(users);
 
-        // check if we have 2 users to start the game. Added exists here cause username gets hit 2 times with same user which causes this to run twice... sloopy code but works so far. 
-        if(users.length === 2){
-            // start the fisrt round. 
-            console.log("first round has started.");
-            aRound();
-        }
    })
 
     socket.on("disconnect", (reason) => {
         console.log("user disconnected - " + reason);
+        // when a user disconnects we need to remove them from the list. 
+        for(i=0; i<users.length; i++){
+            if(users[i].socketID = socket.id){
+                users.splice(i,1)
+            }
+        }
     });
 
 });
